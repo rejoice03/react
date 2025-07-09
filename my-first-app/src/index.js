f9aed280c251f8831e5580c6e7c669fef349a8a9
Rejoice Raju <rejoiceraju03@gmail.com>
2025-07-09 23:25:26 +0530

    added submit button

--- my-first-app/src/index.js
+++ my-first-app/src/index.js
@@ -35,18 +35,26 @@ const BookList = () => {
 };
 
 const EventExample = () => {
-  const handleFormInput = () => {
-    console.log('handle form input')
+  const handleFormInput = (e) => {
+    console.log(e.target);
+    console.log(e.target.name);
+    console.log(e.target.value);
   };
   const handleButtonClick = () => {
     alert('handle button click');
   };
+  const handelFormSubmission = (e) => {
+    e.preventDefault();
+    console.log('form submitted');
+  };
+
   return <section>
     <form>
       <h2>Typical Form</h2>
       <input type='text' name='example' onChange={handleFormInput} style={{margin: '1rem 0'}} />
     </form>
-    <button onClick={handleButtonClick}>Click Me</button>
+    <button type='button'>submit</button>
+    <div><button onClick={handleButtonClick} type='button'>Click Me</button></div>
   </section>
 };
 
