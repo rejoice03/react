9401716fbf0f2274db2c287626d8e2e8e5b9bd14
Rejoice Raju <rejoiceraju03@gmail.com>
2025-07-15 22:51:41 +0530

    completed all the useState

--- 03-advanced-react/src/tutorial/01-useState/starter/04-useState-object.jsx
+++ 03-advanced-react/src/tutorial/01-useState/starter/04-useState-object.jsx
@@ -1,7 +1,27 @@
-import { useState } from 'react';
+import { useState } from "react";
 
 const UseStateObject = () => {
-  return <h2>useState object example</h2>;
+  const [person, setPerson] = useState({
+    name: 'peter',
+    age: 24,
+    hobby: 'reading books',
+  })
+
+  const displayPerson = () => {
+    setPerson({
+      name: 'john',
+      age: 28,
+      hobby: 'scream at the computer',
+    })
+  }
+  return (
+    <>
+      <h3>{person.name}</h3>
+      <h3>{person.age}</h3>
+      <h3>Enjoy: {person.hobby}</h3>
+      <button className="btn" onClick={displayPerson}>Show John</button>
+    </>
+  );
 };
 
 export default UseStateObject;
--- 03-advanced-react/src/tutorial/01-useState/starter/05-useState-gotcha.jsx
+++ 03-advanced-react/src/tutorial/01-useState/starter/05-useState-gotcha.jsx
@@ -1,5 +1,16 @@
+import { useState } from 'react';
 const UseStateGotcha = () => {
-  return <h2>useState "gotcha"</h2>;
+  const [value, setValue] = useState(0);
+  const handleClick = () => {
+    setValue((currentState) => {
+      const newState = currentState +  1;
+      return newState;
+    });
+  };
+  return <div>
+    <h1>{value}</h1>
+    <button type="button" onClick={handleClick} className="btn">Increase</button>
+  </div>;
 };
 
 export default UseStateGotcha;
